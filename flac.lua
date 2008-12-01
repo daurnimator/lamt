c98d4c8f8f5f00259b5983b4b2aa5ff71a34612c
daurnimator <daurnimator@8385cabc-f040-0410-9bc4-65f6fe3c7374>
2008-12-01 15:42:52 +0000

    + Detection for other tagging methods (ID3v1, ID3v2, APE) * Removed space before comma in scrobbler
    git-svn-id: http://lomp.googlecode.com/svn/trunk@52 8385cabc-f040-0410-9bc4-65f6fe3c7374

--- flac.lua
+++ flac.lua
@@ -11,6 +11,8 @@
 
 module ( "lomp.fileinfo.flac" , package.seeall )
 
+require "general"
+
 require "vstruct"
 
 _NAME = "FLAC reader"
@@ -20,9 +22,9 @@ function info ( fd )
 	-- Format info found at http://flac.sourceforge.net/format.html
 	local s = fd:read ( 4 )
 	if s == "fLaC" then 
-		filetyp = "flac"
+		local item = { format = "flac" , extra = { } }
+		
 		local t
-		local sampleinfo = { }
 		local tags = { }
 		repeat
 			t = vstruct.unpack ( "< m1 > u3" , fd )
@@ -31,35 +33,41 @@ function info ( fd )
 			local blocktype = vstruct.implode { unpack ( t [ 1 ] , 1 , 7 ) }		
 			local blocklength = t [ 2 ] -- Is in bytes
 			
+			--print ( lastmetadatablock , blocktype , blocklength )
+			
 			if blocktype == 0 then -- Stream info
 				t = vstruct.unpack ( "> u2 u2 u3 u3 <m8> u16" , fd )
-				sampleinfo.minblocksize = t [ 1 ]
-				sampleinfo.maxblocksize = t [ 2 ]
-				sampleinfo.minframesize = t [ 3 ]
-				sampleinfo.maxframesize = t [ 4 ]
-				sampleinfo.samplerate = vstruct.implode { unpack ( t [ 5 ] , 45 , 64 ) }
-				sampleinfo.channels = vstruct.implode { unpack ( t [ 5 ] , 42 , 44 ) } + 1
-				sampleinfo.bitspersample = vstruct.implode { unpack ( t [ 5 ] , 37 , 41 ) }
-				sampleinfo.totalsamples = vstruct.implode { unpack ( t [ 5 ] , 1 , 36 ) }
-				sampleinfo.md5rawaudio = t [ 6 ]
-				sampleinfo.length = sampleinfo.totalsamples / sampleinfo.samplerate
+				item.extra.minblocksize = t [ 1 ]
+				item.extra.maxblocksize = t [ 2 ]
+				item.extra.minframesize = t [ 3 ]
+				item.extra.maxframesize = t [ 4 ]
+				item.extra.samplerate = vstruct.implode { unpack ( t [ 5 ] , 45 , 64 ) }
+				item.extra.channels = vstruct.implode { unpack ( t [ 5 ] , 42 , 44 ) } + 1
+				item.extra.bitspersample = vstruct.implode { unpack ( t [ 5 ] , 37 , 41 ) }
+				item.extra.totalsamples = vstruct.implode { unpack ( t [ 5 ] , 1 , 36 ) }
+				item.extra.md5rawaudio = t [ 6 ]
+				item.extra.length = item.extra.totalsamples / item.extra.samplerate
 				--t [ 1 ] = nil
 				--samplerate = t [ 5 ]
 				--channels = t [ 6 ] + 1
 				--bitspersample = t [ 7 ]
 				--totalsamples = t [ 8 ]
 			elseif blocktype == 1 then -- Padding
+				item.extra.padding = item.extra.padding or { }
+				table.insert ( item.extra.padding , { start = fd:seek ( ) , length = blocklength , } )
 				t = vstruct.unpack ( "> x" .. blocklength , fd )
 			elseif blocktype == 2 then -- Application
 				t = vstruct.unpack ( "> u4 s" .. ( blocklength - 4 ) , fd )
-				appID = t [ 1 ]
-				appdata = t [ 2 ]
+				item.extra.applications = item.extra.applications or { }
+				table.insert ( item.extra.applications , { appID = t [ 1 ] , appdata = t [ 2 ] } )
 			elseif blocktype == 3 then -- Seektable
 				t = vstruct.unpack ( "> x" .. blocklength , fd ) -- We don't deal with seektables, skip over it
 			elseif blocktype == 4 then -- Vorbis_Comment http://www.xiph.org/vorbis/doc/v-comment.html
+				item.tagtype = "vorbis"
+				item.extra.startvorbis = fd:seek ( ) - 4
 				t = vstruct.unpack ( "< u4" , fd )
 				vendor_length = t [ 1 ]
-				t = vstruct.unpack ( "<s" .. vendor_length .. "u4" , fd )
+				t = vstruct.unpack ( "< s" .. vendor_length .. "u4" , fd )
 				vendor_string = t [ 1 ]
 				user_comment_list_length = t [ 2 ]
 				
@@ -74,7 +82,6 @@ function info ( fd )
 					tags [ fieldname ] = tags [ fieldname ] or { }
 					table.insert ( tags [ fieldname ] , value )
 				end
-				
 			elseif blocktype == 5 then -- Cuesheet
 				t = vstruct.unpack ( "> s128 u8 x259 x1 x" .. ( blocklength - ( 128 + 8 + 259 + 1 ) ) , fd ) -- cbf, TODO: cuesheet reading
 			elseif blocktype == 6 then -- Picture
@@ -94,14 +101,44 @@ function info ( fd )
 				picturedata = t [ 1 ]
 			end
 		until lastmetadatablock == 1
-		local item = { }
-		item.format = "flac"
-		item.length = math.floor( sampleinfo.length + 0.5 )
+		item.length = math.floor( item.extra.length + 0.5 )
 		item.tags = tags or { }
-		item.extra = sampleinfo
 		return item
 	else
 		-- not a flac file
 		return false
+	end
+end
+
+function edit ( fd , tags )
+	local item = info ( fd )
+	
+	local vendor_string = ""
+	local vendor_length = string.len ( vendor_string )
+	
+	local commentcount = 0
+	local s = ""
+	for k , v in ipairs ( tags ) do
+		for i , v in ipairs ( v ) do
+			commentcount = commentcount + 1
+			local comment = k .. "=" .. v
+			local length = string.len ( comment )
+			s = s .. vstruct.pack ( "u4 s" , length , comment )
+		end
+	end
+	
+	s = vstruct.pack ( "u4 s u4" , vendor_length , vendor_string , commentcount ) .. s
+	local length = string.len ( s )
+	s = vstruct.pack ( "u3" , length ) .. s
+	
+	local space_needed = string.len ( s )
+	
+	local oldblocksize = 0
+	if item.extra.startvorbis then
+		fd:seek ( item.extra.startvorbis + 1 )
+		oldblocksize = vstruct.unpack ( "u3" , fd ) [ 1 ]
+	end
+	
+	if space_needed == oldblocksize then
 	end
 end
