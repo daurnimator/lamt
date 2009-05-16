0f0a694bcd69198bda83b87f590d89ef0bf9ae75
daurnimator <daurnimator@8385cabc-f040-0410-9bc4-65f6fe3c7374>
2009-05-16 05:34:02 +0000

    * Added more id3v2 frames + string.explode added to general.lua + moved id3v1 genre list to seperate file
    git-svn-id: http://lomp.googlecode.com/svn/trunk@75 8385cabc-f040-0410-9bc4-65f6fe3c7374

--- id3v2.lua
+++ id3v2.lua
@@ -16,6 +16,7 @@ module ( "lomp.fileinfo.id3v2" , package.see ( lomp ) )
 pcall ( require , "luarocks.require" ) -- Activates luarocks if available.
 require "vstruct"
 require "iconv"
+local genrelist = require "modules.fileinfo.genrelist"
 
 _NAME = "ID3v2 tag reader/writer"
 -- http://www.id3.org/id3v2.4.0-structure
@@ -30,6 +31,11 @@ local function desafesync ( tbl )
 	return vstruct.implode ( new )
 end
 
+local function utf8 ( str , encoding )
+	if not encoding then encoding = "ISO-8859-1" end
+	return iconv.new ( "UTF-8" ,  encoding ):iconv ( str )
+end
+
 local function readheader ( fd )
 	local t = vstruct.unpack ( "> ident:s3 version:u1 revision:u1 flags:m1 safesyncsize:m4" , fd )
 	if ( t.ident == "ID3" or t.ident == "3DI" ) then
@@ -42,15 +48,15 @@ local function readheader ( fd )
 end
 
 local frameencode = {
-	["uniquefileid"] = "UFID" ,
-	["content group"] = "TIT1" ,
-	["title"] = "TIT2" ,
-	["subtitle"] = "TIT3" ,
-	["album"] = "TALB" ,
-	["original album"] = "TOAL" ,
-	["tracknumber"] = function ( item )
+	[ "uniquefileid" ] = "UFID" ,
+	[ "content group" ] = "TIT1" ,
+	[ "title" ] = "TIT2" ,
+	[ "subtitle" ] = "TIT3" ,
+	[ "album" ] = "TALB" ,
+	[ "original album" ] = "TOAL" ,
+	[ "tracknumber" ] = function ( item )
 						local t = { }
-						for i , v in ipairs ( item ["tracknumber"] ) do
+						for i , v in ipairs ( item ["tracknumber"] or { } ) do
 							if item ["totaltracks"] [i] then
 								table.insert ( t , tostring ( v ) .. "/" .. item ["totaltracks"] [i] )
 							else
@@ -59,29 +65,51 @@ local frameencode = {
 						end
 						return "TRCK" , t
 					end ,
-	["totaltracks"] = function ( item )
+	[ "totaltracks" ] = function ( item )
 						if not item ["tracknumber"] [i] then -- If not going to put in with tracknumber in TRCK, put in TXXX
 							return "TXXX"
 						end
 					end ,
-	["discnumber"] = function ( item )
+	[ "discnumber" ] = function ( item )
 						local t = { }
-						for i , v in ipairs ( item ["tracknumber"] ) do
-							if item ["totaltracks"] [i] then
-								table.insert ( t , tostring ( v ) .. "/" .. item ["totaltracks"] [i] )
+						for i , v in ipairs ( item ["discnumber"] or { } ) do
+							if item ["totaldiscs"] [i] then
+								table.insert ( t , tostring ( v ) .. "/" .. item ["totaldiscs"] [i] )
 							else
 								table.insert ( t , tostring ( v ) ) 
 							end	
 						end
 						return "TPOS" , t
 					end ,	
-	["totaldiscs"] = function ( item )
+	[ "totaldiscs" ] = function ( item )
 						if not item ["discnumber"] [i] then -- If not going to put in with discnumber in TPOS, put in TXXX
 							return "TXXX"
 						end
 					end ,
-	["set subtitle"] = "TSST" ,
-	["isrc"] = "TSRC" ,
+	[ "set subtitle" ] = "TSST" ,
+	[ "isrc" ] = "TSRC" ,
+
+	[ "artist" ] = "TPE1" ,
+	[ "band" ] = "TPE2" ,
+	[ "conductor" ] = "TPE3" , 
+	[ "remixed by" ] = "TPE4" ,
+	[ "original artist" ] = "TOPE" ,
+	[ "writer" ] = "TEXT" ,
+	[ "original writer" ] = "TOLY" ,
+	[ "composer" ] = "TCOM" ,
+	-- TODO: TMCL, TIPL (lists)
+	[ "encoded by" ] = "TENC" ,
+	
+	
+	
+	[ "commercial information url" ] = "WCOM" ,
+	[ "copyright url" ] = "WCOP" ,
+	[ "file webpage url" ] = "WOAF" ,
+	[ "artist webpage url" ] = "WOAR" ,
+	[ "source webpage url" ] = "WOAS" , 
+	[ "internet radio webpage url" ] = "WORS" , 
+	[ "payment url" ] = "WPAY" , 
+	[ "publisher url" ] = "WPUB" ,
 }
 
 local encodings = {
@@ -93,7 +121,11 @@ local encodings = {
 
 local function readtextframe ( str )
 	local t = vstruct.unpack ( "> encoding:u1 text:s" , str )
-	return iconv.new ( "UTF-8" , encodings [ t.encoding ].name ):iconv ( t.text ) )
+	local st = string.explode ( t.text )
+	for i , v in ipairs ( st ) do
+		st [ i ] = utf8 ( v , encodings [ t.encoding ].name )
+	end
+	return st
 end
 
 local framedecode = {
@@ -102,6 +134,7 @@ local framedecode = {
 		end ,
 		
 	-- TEXT fields
+	-- Identification frames
 	["TIT1"] = function ( str ) -- Content group description
 			return { [ "content group" ] = readtextframe ( str ) }
 		end ,
@@ -118,11 +151,17 @@ local framedecode = {
 			return { [ "original album" ] = readtextframe ( str ) }
 		end ,
 	["TRCK"] = function ( str ) -- Track number/Position in set
-			local track , total = string.match ( readtextframe ( str ) , "([^/*])/?(.-*)" )
+			local track , total = { } , { }
+			for i , v in ipairs ( readtextframe ( str ) ) do
+				track [ #track + 1 ] , total [ #total + 1 ] = string.match ( v , "([^/*])/?(.-*)" )
+			end
 			return { [ "tracknumber" ] = track , ["totaltracks"] = total }
 		end ,
 	["TPOS"] = function ( str ) -- Part of a set
-			local disc , total = string.match ( readtextframe ( str ) , "([^/*])/?(.-*)" )
+			local disc , total = { } , { }
+			for i , v in ipairs ( readtextframe ( str ) ) do
+				disc [ #disc + 1 ] , total [ #total + 1 ] = string.match ( v , "([^/*])/?(.-*)" )
+			end
 			return { [ "discnumber" ] = disc , ["totaldiscs"] = total }
 		end ,
 	["TSST"] = function ( str ) -- Set subtitle
@@ -131,34 +170,122 @@ local framedecode = {
 	["TSRC"] = function ( str ) -- ISRC
 			return { [ "ISRC" ] = readtextframe ( str ) }
 		end ,
+	-- Involved persons frames
+	["TPE1"] = function ( str ) -- Lead artist/Lead performer/Soloist/Performing group
+		return { [ "artist" ] = readtextframe ( str ) }
+	end ,
+	["TPE2"] = function ( str ) -- Band/Orchestra/Accompaniment
+		return { [ "band" ] = readtextframe ( str ) }
+	end ,
+	["TPE3"] = function ( str ) -- Conductor
+		return { [ "conductor" ] = readtextframe ( str ) }
+	end ,
+	["TPE4"] = function ( str ) -- Interpreted, remixed, or otherwise modified by
+		return { [ "remixed by" ] = readtextframe ( str ) }
+	end ,
+	["TOPE"] = function ( str ) -- Original artist/performer
+		return { [ "original artist" ] = readtextframe ( str ) }
+	end ,
+	["TEXT"] = function ( str ) -- Lyricist/Text writer
+		return { [ "writer" ] = readtextframe ( str ) }
+	end ,
+	["TOLY"] = function ( str ) -- Original lyricist/text writer
+		return { [ "original writer" ] = readtextframe ( str ) }
+	end ,
+	["TCOM"] = function ( str ) -- Composer
+		return { [ "composer" ] = readtextframe ( str ) }
+	end ,
+	["TMCL"] = function ( str ) -- Musician credits list
+		local t , field = {} , ""
+		for i , v in ipairs ( readtextframe ( str ) ) do
+			if i % 2 == 1 then -- odd, field is instrument
+				field = v .. " player"
+				t [ field ] = t [ field ] or { }
+			else -- even, is musician's name
+				t [ field ] [ #t [ field ] ] = v
+			end
+		end
+		return t
+	end ,
+	["TIPL"] = function ( str ) -- Involved people list
+		local t , field = {} , ""
+		for i , v in ipairs ( readtextframe ( str ) ) do
+			if i % 2 == 1 then -- odd, field is instrument
+				field = v
+				t [ field ] = t [ field ] or { }
+			else -- even, is musician's name
+				t [ field ] [ #t [ field ] ] = v
+			end
+		end
+		return t
+	end ,
+	["TENC"] = function ( str ) -- Encoded by
+		return { [ "encoded by" ] = readtextframe ( str ) }
+	end ,
+	-- Derived and subjective properties frames
+	["TBPM"] = function ( str ) -- BPM
+		return { [ "bpm" ] = readtextframe ( str ) }
+	end ,
+	["TLEN"] = function ( str ) -- Length
+		return { [ "length" ] = readtextframe ( str ) }
+	end ,
+	["TKEY"] = function ( str ) -- Initial key
+		return { [ "musical key" ] = readtextframe ( str ) }
+	end ,
+	["TLAN"] = function ( str ) -- Language
+		return { [ "language" ] = readtextframe ( str ) }
+	end ,
+	["TCON"] = function ( str ) -- Content type
+		local t = readtextframe ( str )
+		for k , v in ipairs ( t) do
+			if v == "RX" then t [ k ] = "Remix"
+			elseif v == "CR" then t [ k ] = "Cover" 
+			elseif tonumber ( v ) then
+				t [ k ] = genrelist [ tonumber ( v ) ]
+			end
+		end
+		return { [ "genre" ] = t }
+	end ,
+	["TFLT"] = function ( str ) -- File Type
+		return { [ "FIELD" ] = readtextframe ( str ) }
+	end ,
+	["TXXX"] = function ( str ) -- 
+		return { [ "FIELD" ] = readtextframe ( str ) }
+	end ,
+	["TXXX"] = function ( str ) -- 
+		return { [ "FIELD" ] = readtextframe ( str ) }
+	end ,
+	["TXXX"] = function ( str ) -- 
+		return { [ "FIELD" ] = readtextframe ( str ) }
+	end ,
 	
 	-- URL fields,
 	["WCOM"] = function ( str ) -- Commerical information
-		return { buyurl = str }
+		return { ["commercial information url"] = str }
 	end ,
 	["WCOP"] = function ( str ) -- Copyright/Legal information
-		return { buyurl = str }
+		return { ["copyright url"] = str }
 	end ,
 	["WOAF"] = function ( str ) -- Official audio file webpage
-		return { buyurl = str }
+		return { ["file webpage url"] = str }
 	end ,
 	["WOAR"] = function ( str ) -- Official artist/performer webpage
-		return { buyurl = str }
+		return { ["artist webpage url"] = str }
 	end ,
 	["WOAS"] = function ( str ) -- Official audio source webpage
-		return { buyurl = str }
+		return { ["source webpage url"] = str }
 	end ,
 	["WORS"] = function ( str ) -- Official Internet radio station homepage
-		return { buyurl = str }
+		return { ["internet radio webpage url"] = str }
 	end ,
 	["WPAY"] = function ( str ) -- Payment
-		return { buyurl = str }
+		return { ["payment url"] = str }
 	end ,
 	["WPUB"] = function ( str ) -- Publishers official webpage
-		return { buyurl = str }
+		return { ["publisher url"] = str }
 	end ,
 	["WXXX"] = function ( str ) -- Custom
-		return { buyurl = str }
+		return { ["custom url"] = str }
 	end ,	
 	
 }
