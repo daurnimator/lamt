a61aca5908a486b304524053f0226838b2cfab29
daurnimator <quae@daurnimator.com>
2011-11-23 15:21:40 +1100

    + Simplify; don't take new tags and old tags; but rather a table of edits.

--- vorbiscomments.lua
+++ vorbiscomments.lua
@@ -1,4 +1,6 @@
--- Vorbis_Comment http://www.xiph.org/vorbis/doc/v-comment.html
+-- Vorbis_Comment
+-- http://www.xiph.org/vorbis/doc/v-comment.html
+-- https://wiki.xiph.org/VorbisComment
 
 local error = error
 local ipairs , pairs , rawset = ipairs , pairs , rawset
@@ -28,40 +30,20 @@ local function read ( get , tags , extra )
 	return tags , extra
 end
 
-local function generate ( edits , oldtags , extra , exact )
+local function generate ( edits , extra , exact )
 	vendor_string = ( extra and extra.vendor_string ) or "lamt vorbiscomment" --"Xiph.Org libVorbis I 20020717"
 
-	local comments = setmetatable ( { } , {
-			__newindex = function ( t , k , v )
-				-- k_clean is "A case-insensitive field name that may consist of ASCII 0x20 through 0x7D, 0x3D ('=') excluded."
-				local k_clean = strgsub ( k , "[=%z\1-\31\127-\255]" , "" )
-				k_clean = k_clean:lower ( )
-
-				if k_clean ~= k and exact then
-					error ( "Invalid field name" , 2 )
-				end
-
-				return rawset ( t , k_clean , v )
-			end ;
-		} )
-
-	-- Merge edits:
-	if oldtags then
-		for k , v in pairs ( oldtags ) do
-			for i , vv in ipairs ( v ) do -- Copy the table
-				tblinsert ( comments [ k ] , vv )
-			end
-		end
-	end
-
-	for k , v in pairs ( edits ) do -- edits overwrite any old tags with the given key...
-		comments [ k ] = v
-	end
-
 	local tbl = { }
-	for k , v in pairs ( comments ) do
+	for k , v in pairs ( edits ) do
+		local k_clean = strgsub ( k , "[=%z\1-\31\127-\255]" , "" )
+		k_clean = k_clean:lower ( )
+
+		if k_clean ~= k and exact then
+			error ( "Invalid field name" , 2 )
+		end
+
 		for i , v in ipairs ( v ) do
-			local vector = k .. "=" .. v
+			local vector = k_clean .. "=" .. tostring ( v )
 			tblinsert ( tbl , num_to_le_uint ( #vector ) .. vector )
 		end
 	end
