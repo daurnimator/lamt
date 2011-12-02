0a3d58c1ea07204aa7ba66ff180a37e16be4b749
daurnimator <quae@daurnimator.com>
2011-12-02 21:14:23 +1100

    + ll.lua - Remove ffi specific stuff; add extract_bits & bpeek

--- ll.lua
+++ ll.lua
@@ -1,44 +1,73 @@
 -- Library for reading low level data
 
-local hasffi , ffi = pcall ( require , "ffi" )
-local hasbit , bit = pcall ( require , "bit" )
+local assert = assert
+local unpack = unpack
+local floor = math.floor
+local strbyte , strchar = string.byte , string.char
 
 local ll = { }
 
-if hasffi and hasbit then
-	local uint_to_num = function ( s )
-		return ffi.cast ( "uint32_t*" , s )[0]
+local le_uint_to_num = function ( s , i , j )
+	i , j = i or 1 , j or -1
+	local b = { strbyte ( s , i , j ) }
+	local n = 0
+	for i=#b , 1 , -1 do
+		n = n*2^8 + b [ i ]
 	end
-
-	local num_to_uint = function ( n )
-		return ffi.string ( ffi.new ( "uint32_t[1]" , n ) , 4 )
+	return n
+end
+local num_to_le_uint = function ( n , bytes )
+	bytes = bytes or 4
+	local b = { }
+	for i=1 , bytes do
+		b [ i ] , n = n % 2^8 , floor ( n / 2^8 )
 	end
-
-	if ffi.abi ( "le" ) then
-		ll.le_uint_to_num = uint_to_num
-		ll.num_to_le_uint = num_to_uint
-	elseif ffi.abi ( "be" ) then
-		ll.le_uint_to_num = function ( s ) return bit.bswap ( uint_to_num ( s ) ) end
-		ll.num_to_le_uint = function ( n ) return num_to_uint ( bit.bswap ( n ) ) end
-	else error ( "Unknown endianess" ) end
-else
-	local strbyte = string.byte
-	local strchar = string.char
-	local floor = math.floor
-
-	ll.le_uint_to_num = function ( s )
-		local b1 , b2 , b3 , b4 = strbyte ( s , 1 , 4 )
-		return b4*2^24 + b3*2^16 + b2*2^8 + b1
+	assert ( n == 0 )
+	return strchar ( unpack ( b ) )
+end
+local be_uint_to_num = function ( s , i , j )
+	i , j = i or 1 , j or -1
+	local b = { strbyte ( s , i , j ) }
+	local n = 0
+	for i=1 , #b do
+		n = n*2^8 + b [ i ]
 	end
-	ll.num_to_le_uint = function ( n )
-		local b1 , b2 , b3 , b4
-		b1 , n = n % 2^8 , floor ( n / 2^8 )
-		b2 , n = n % 2^8 , floor ( n / 2^8 )
-		b3 , n = n % 2^8 , floor ( n / 2^8 )
-		b4 , n = n % 2^8 , floor ( n / 2^8 )
-		assert ( n == 0 )
-		return strchar ( b1 , b2 , b3 , b4 )
+	return n
+end
+local num_to_be_uint = function ( n , bytes )
+	bytes = bytes or 4
+	local b = { }
+	for i=bytes , 1 , -1 do
+		b [ i ] , n = n % 2^8 , floor ( n / 2^8 )
 	end
+	assert ( n == 0 )
+	return strchar ( unpack ( b ) )
 end
 
-return ll
+-- Returns (as a number); bits i to j (indexed from 0)
+local extract_bits = function ( s , i , j )
+	j = j or i
+	local i_byte = floor ( i / 8 ) + 1
+	local j_byte = floor ( j / 8 ) + 1
+
+	local n = be_uint_to_num ( s , i_byte , j_byte )
+	n = n % 2^( j_byte*8 - i )
+	n = floor ( n / 2^( (-(j+1) ) % 8 ) )
+	return n
+end
+
+-- Look at ith bit in given string (indexed from 0)
+-- Returns boolean
+local bpeek = function ( s , bitnum )
+	return extract_bits ( s , bitnum ) == 1
+end
+
+return {
+	le_uint_to_num = le_uint_to_num ;
+	num_to_le_uint = num_to_le_uint ;
+	be_uint_to_num = be_uint_to_num ;
+	num_to_be_uint = num_to_be_uint ;
+
+	extract_bits = extract_bits ;
+	bpeek = bpeek ;
+}
